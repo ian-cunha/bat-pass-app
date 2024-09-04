6ed0d3c6d9e4377d127ede01a7419bdbdfb700c8
Ian <iancunha20@gmail.com>
2024-09-03 22:57:10 -0300

    update

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import styles from './Style';
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>Home works!</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
